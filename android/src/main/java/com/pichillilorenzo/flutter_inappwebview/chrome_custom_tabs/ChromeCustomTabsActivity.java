3d98540cfac3d7b75eb9dbe12cb095ef01aaec2b
mayank4741 <80269518+mayank4741@users.noreply.github.com>
2022-09-12 14:20:16 -0400

    Adding FLAG_SECURE

--- android/src/main/java/com/pichillilorenzo/flutter_inappwebview/chrome_custom_tabs/ChromeCustomTabsActivity.java
+++ android/src/main/java/com/pichillilorenzo/flutter_inappwebview/chrome_custom_tabs/ChromeCustomTabsActivity.java
@@ -53,16 +53,7 @@ public class ChromeCustomTabsActivity extends Activity implements MethodChannel.
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    if(!BuildConfig.DEBUG) {
-      SharedPreferences preferences = context.getSharedPreferences(FLAG_SECURE_PREFS, Context.MODE_PRIVATE);
-      if (preferences != null) {
-        boolean secureFlag = preferences.getBoolean(FLAG_SECURE_VALUE, true);
-        if(secureFlag)
-          getWindow().setFlags(LayoutParams.FLAG_SECURE, LayoutParams.FLAG_SECURE);
-      } else {
-        getWindow().setFlags(LayoutParams.FLAG_SECURE, LayoutParams.FLAG_SECURE);
-      }
-    }
+    getWindow().setFlags(LayoutParams.FLAG_SECURE, LayoutParams.FLAG_SECURE);
 
     setContentView(R.layout.chrome_custom_tabs_layout);
 
